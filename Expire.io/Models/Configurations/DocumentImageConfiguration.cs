ed9bf3414ddce61ba3ced3d05d541c4038cea0b5
mezzo_soprano <svyatkokatola@gmail.com>
2019-04-03 10:33:29 +0300

    Models Configuration added

---
+++ Expire.io/Models/Configurations/DocumentImageConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UniversityProj.Models.Entities;
+
+namespace UniversityProj.Models.Configurations
+{
+    public class DocumentImageConfiguration : IEntityTypeConfiguration<DocumentImage>
+    {
+        public void Configure(EntityTypeBuilder<DocumentImage> builder)
+        {
+            builder.ToTable("DocumentImage");
+            builder.HasKey(d => d.Id);
+
+            builder.HasOne(d => d.Document).WithOne(d => d.Image).HasForeignKey<DocumentImage>(d=>d.DocumentId).IsRequired();
+
+            builder.Property(d => d.Image).IsRequired(true);
+        }
+    }
+}
---
+++ Expire.io/Models/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniversityProj.Models.Entities;
+
+namespace UniversityProj.Models.Configurations
+{
+    public class UserRoleConfiguration: IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            builder.HasIndex(r => new { r.RoleId, r.UserId }).IsUnique();
+
+            builder.HasOne(r => r.User).WithMany(u => u.UserRoles)
+                .HasForeignKey(r => r.UserId).IsRequired();
+
+            builder.HasOne(r => r.Role).WithMany(r => r.UserRoles)
+                .HasForeignKey(r => r.RoleId).IsRequired();
+        }
+
+         
+    }
+}

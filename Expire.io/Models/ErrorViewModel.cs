cf4618acf998011f9ad01ceb5d442539c851c99f
Taras Kataryna <tarikkataryna1999@gmail.com>
2019-04-03 08:27:27 +0300

    First commit

---
+++ Expire.io/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Expire.io.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

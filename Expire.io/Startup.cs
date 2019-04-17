9b6c88b00612bab66c757037bb7d377f11308afd
Taras Kataryna <tkata@softserveinc.com>
2019-04-17 13:29:38 +0300

    Added

--- Expire.io/Startup.cs
+++ Expire.io/Startup.cs
@@ -52,15 +52,15 @@ namespace Expire.io
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
-            {
+            {*/
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
-            }
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();

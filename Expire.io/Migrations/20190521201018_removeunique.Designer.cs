21e0915c24c0d244314caee24743060ea7893141
Taras Kataryna <tkata@softserveinc.com>
2019-05-21 23:38:34 +0300

    add color line

--- Expire.io/Migrations/20190521201018_removeunique.Designer.cs
+++ Expire.io/Migrations/20190521201018_removeunique.Designer.cs
@@ -4,14 +4,16 @@ using Expire.io.Models.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Expire.io.Migrations
 {
     [DbContext(typeof(ExpireContext))]
-    partial class ExpireContextModelSnapshot : ModelSnapshot
+    [Migration("20190521201018_removeunique")]
+    partial class removeunique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
